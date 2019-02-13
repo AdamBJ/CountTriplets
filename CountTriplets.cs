c292d0164e95bd3769a6bbc6cd0f4686c62a3127
Adam Bolding-Jones <anb1@sfu.ca>
2019-02-12 21:33:54 -0800

    Pain

--- CountTriplets.cs
+++ CountTriplets.cs
@@ -13,40 +13,130 @@ using System.Text;
 using System;
 
 class CountTriplets {
-    private struct Info
-    {
-        public List<long>occurenceIdxs;
-
-        public Info(int idx)
-        {
-            this.occurenceIdxs = new List<long>();
-            this.occurenceIdxs.Add(idx);
-        }
-    }
-
-    // Complete the countTriplets function below.
     static long countTriplets(List<long> arr, long r) {
-        Dictionary<long, Info> dict = new Dictionary<long, Info>();
+        // Precompute occurences of all integers in arr
+        Dictionary<long, List<long>> dict = new Dictionary<long, List<long>>();
         for (int i=0; i<arr.Count; i++)
         {
             long current = arr[i];
             if (dict.ContainsKey(current))
             {
-                Info currInfo = dict[current];
-                currInfo.occurenceIdxs.Add(i);
-                dict[current] = currInfo;
+                List<long> currOccurenceIdxs = dict[current];
+                currOccurenceIdxs.Add(i);
+                dict[current] = currOccurenceIdxs;
             }
             else
             {
-                dict.Add(current, new Info(i));
+                dict.Add(current, new List<long>(){i});
             }
         }
 
-        return 1;
+        long numTriplets = 0;
+        bool shouldContinue = false;
+        foreach (KeyValuePair<long, List<long>> entry in dict)
+        {
+            if (shouldContinue)
+            {
+                shouldContinue = false;
+                continue;
+            }
+            long first = entry.Key;
+            List<long> firstOccurenceIdxs = entry.Value;
+            foreach(long firstOccurenceIdx in firstOccurenceIdxs)
+            {
+                long second = first*r;
+                if (!dict.ContainsKey(second))
+                {
+                    // No way to continue to triple
+                    break;
+                }
+                else
+                {
+                    List<long> secondOccurenceIdxs = dict[second];
+                    for(int i=secondOccurenceIdxs.Count-1; i>=0; i--)
+                    {   
+                        long secondOccurenceIdx = secondOccurenceIdxs[i];
+                        if (secondOccurenceIdx <= firstOccurenceIdx)
+                        {
+                            // To the left, not valid triple
+                            break;
+                        }
 
+                        long third = second*r;
+                        if (!dict.ContainsKey(third))
+                        {
+                            // No way to continue to triple
+                            shouldContinue = true;
+                            break;
+                        } 
+                        else
+                        {
+                            List<long> thirdOccurenceIdxs = dict[third];
+                            for(int j=thirdOccurenceIdxs.Count-1; j>=0; j--)
+                            {
+                                long thirdOccurenceIdx = thirdOccurenceIdxs[j];
+                                if (thirdOccurenceIdx <= secondOccurenceIdx)
+                                {
+                                    // To the left, not valid triple
+                                    break;
+                                }
+                                // Found valid triple
+                                numTriplets++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return numTriplets;
     }
 
 
+        // for(int i=0; i<arr.Count; i++)
+        // {
+        //     long first = arr[i];
+        //     long second = first * r;
+        //     if (!dict.ContainsKey(second))
+        //     {
+        //         continue;
+        //     } 
+        //     else
+        //     {
+        //         long third = second * r;
+        //         if (!dict.ContainsKey(third))
+        //         {
+        //             continue;
+        //         }
+        //         else
+        //         {
+        //             // We found a triplet pattern. Count number occurences.
+        //             List<long> firstOccurenceIdxs = dict[first];
+        //             List<long> secondOccurenceIdxs = dict[second];
+        //             List<long> thirdOccurenceIdxs = dict[third];
+        //             // Fundamental principle of counting, x ways to pick first item, y ways to pick second item, z way to pick third -> x*y*z ways to pick three items.
+        //             numTriplets = 
+        //                 getNumPickable(i, firstOccurenceIdxs) * getNumPickable(i, secondOccurenceIdxs) * getNumPickable(i, thirdOccurenceIdxs);
+        //         }
+        //     }
+        // }
+        
+
+    // private static long getNumPickable(int i, List<long> occurenceIdxs)
+    // {
+    //     // TODO go over this backwards. That way we can short-circuit execution once we find an index < i
+    //     long numPickable = 0;
+    //     foreach(long occurenceIdx in occurenceIdxs)
+    //     {
+    //         if (occurenceIdx < i)
+    //         {
+    //             continue;
+    //         }
+    //         numPickable++;
+    //     }
+
+    //     return numPickable;
+    // }
 
     static void Main(string[] args) {
         //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
